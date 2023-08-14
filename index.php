bd68235e1b8dbcd0fdd8682f1e9aedb93aef402d
dondjinglandiao <120798345+dondjinglandiao@users.noreply.github.com>
2023-08-14 22:29:38 +0800

    添加验证码验证

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     echo '<h1 class="title">WhaleBlue ShortLink</h1>';
     echo '<form method="post" action="">';
     echo '<input type="text" name="long_url" placeholder="http(s)://wlanu.com/…" required>';
+    echo '<input type="text" name="captcha" placeholder="验证码" required>';
+    echo '<img src="/captcha.php" alt="验证码">';
     echo '<input type="submit" value="生成">';
     echo '</form>';
     echo '</div>';
@@ -21,7 +23,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $longUrl = $_POST['long_url'];
     $ip = $_SERVER['REMOTE_ADDR'];
-
+    $captcha = $_POST['captcha'];
+    
+    session_start();
+    if ($_SESSION['captcha'] !== $captcha) {
+        echo '<html>';
+        echo '<head>';
+        echo '<title>WhaleBlue ShortLink</title>';
+        echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+        echo '<link rel="stylesheet" href="/css/main-styles.css">';
+        echo '</head>';
+        echo '<body>';
+        echo '<div class="card">';
+        echo '<h1 class="title">验证码错误</h1>';
+        echo '<form method="post" action="">';
+        echo '<input type="text" name="long_url" placeholder="http(s)://wlanu.com/…" required>';
+        echo '<input type="text" name="captcha" placeholder="验证码" required>';
+        echo '<img src="captcha.php" alt="验证码">';
+        echo '<input type="submit" value="生成">';
+        echo '</form>';
+        echo '</div>';
+        echo '</body>';
+        echo '</html>';
+        exit();
+    }
+    
     $random = generateRandomString(5);
 
     $shortUrl = 'https://' . $_SERVER['HTTP_HOST'] . '/' . $random;
